7f8f6959d1962da88cd70b70056464487c70f081
Aleksa <72038770+AlesyaRabushka@users.noreply.github.com>
2022-12-08 21:07:02 +0300

    first try

---
+++ main.py
@@ -0,0 +1,36 @@
+from neural_network import *
+
+if __name__ == '__main__':
+
+    while(True):
+        print('Выберите действие:')
+        print('1 - Обучение')
+        print('2 - Распознавание')
+        menu = (input())
+
+
+        if int(menu) == 1:
+            file_name = input('Введите название файла с образами ')
+            models = read_model_from_file(file_name)
+            matrix = get_weight_matrix_from_models(models)
+            for i in matrix:
+                print(i)
+            print('Сохранить матрицу весов? ')
+            print('1 - Да\n2 - Нет')
+            choice = int(input())
+            if choice == 1:
+                write_into_file('matrix.txt', matrix)
+            else:
+                pass
+
+
+        elif int(menu) == 2:
+            model_file_name = input('Введите название файла с образом ')
+            matrix_file_name = input('Введите название файла с матрицей весов ')
+            matrix = read_matrix_from_file(matrix_file_name)
+            model = read_model_from_file(model_file_name)
+
+            recognize(model, matrix)
+
+        else:
+            break
---
+++ neural_network.py
@@ -0,0 +1,66 @@
+from functions import *
+import functools
+
+
+# def hopfild_neural_network(input_sequence, weight_matrix):
+#     X = transform_into_matrix(input_sequence)
+#     X_ = matrix_transposition(X)
+#
+#
+#     if not weight_matrix:
+#         #weight_matrix = get_weight_matrix(X)
+#         weight_matrix = matrix_multiplication(X, X_)
+#         weight_matrix = zero_main_diagonal(weight_matrix)
+#
+#     training(X, weight_matrix)
+
+
+# def training(X, weight_matrix):
+#     previous_state = X
+#     current_state = []
+#     relax = False
+#
+#     iteration = 0
+#     while(iteration <= 1000):
+#         state_matrix = transform_into_matrix(previous_state)
+#
+#         current_state = activation_function(matrix_multiplication(weight_matrix, previous_state), previous_state)
+#         if functools.reduce(lambda x, y: x and y, map(lambda a, b: a == b, previous_state, current_state), True):
+#             relax = True
+#             print('Состояние релаксации достигнуто!')
+#             write_matrix_into_file(weight_matrix)
+#             break
+#         iteration += 1
+#         previous_state = current_state
+#     if not relax:
+#         print('Состояние релаксации не достигнуто!')
+
+
+
+
+
+
+def recognize(X, weight_matrix):
+    previous_state = matrix_transposition(X)
+    curent_state = []
+    relax = False
+    iteration = 0
+
+    while relax is not True:
+        if iteration >= 1000:
+            print('Невозможно распознать образ!')
+            print(matrix_transposition(previous_state))
+            print(matrix_transposition(current_state))
+            exit()
+
+        m = matrix_multiplication(weight_matrix, (previous_state))
+
+        current_state = activation_function(m, previous_state)
+
+        if functools.reduce(lambda x, y: x and y, map(lambda a, b: a == b, previous_state, current_state), True):
+            print('Образ распознан!')
+            show_model(matrix_transposition(current_state))
+            relax = True
+
+        previous_state = (current_state)
+        iteration += 1
